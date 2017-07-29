c3ef8aa6563356bdd91da48517b66779e40d0ff6
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-07-29 21:50:46 +0200

    +storage service

---
+++ src/libs/storage.js
@@ -0,0 +1,39 @@
+export class LocalStorageFactory {
+  /**
+   * @param {string} componentName
+   * @returns {StorageProxy}
+   */
+  static create(componentName) {
+    return new StorageProxy(componentName, window.localStorage);
+  }
+}
+
+/**
+ * @private
+ */
+export class StorageProxy {
+  /**
+   * @param componentName
+   * @param {Storage} storage
+   */
+  constructor(componentName, storage) {
+    /** @private */
+    this.key = componentName;
+    /** @private */
+    this.storage = storage;
+  }
+
+  get(defaultValue) {
+    const value = JSON.parse(this.storage.getItem(this.key));
+    console.log(value);
+    return value === null ? defaultValue : value;
+  }
+
+  set(value) {
+    this.storage.setItem(this.key, JSON.stringify(value));
+  }
+
+  remove(key) {
+    this.storage.removeItem(this.key)
+  }
+}

2d9471703b2b263f4e871e350dd0a1931d85962c
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-07-30 11:55:43 +0200

    extract showcase component, open chanel from url

--- src/app.js
+++ src/app.js
@@ -1,96 +1,32 @@
 import React, { Component } from 'react';
-import styles from './app.scss';
-import { VideoPlayer } from './components/video-player/video-player';
-import { ChannelListMode } from './components/list-switcher/channel-list-modes';
-import { Playlist } from './entities/playlist.model';
-import { FavouritesEditor } from './components/favourites-editor/favourites-editor';
-import { ChannelsPanel } from './components/channels-panel/channels-panel';
 import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom'
-import { LocalStorageFactory } from './libs/storage';
+import { Showcase } from './components/showcase/showcase';
 
 class App extends Component {
-  favouritesStorage = LocalStorageFactory.create('favourites');
 
   state = {
-    currentChannel: null,
-    favourites: this.favouritesStorage.get([]),
+    playlistUrl: 'http://localhost:3001/playlist?url=myott.tv',
     currentKey: '00XE8DMEI7',
-    channels: [],
   };
 
-  componentWillMount() {
-    this.loadPlaylist().then((playlist) => {
-      this.setState({
-        channels: playlist.channels,
-      })
-    })
-  }
-
-  loadPlaylist() {
-    const url = 'http://localhost:3001/playlist?url=myott.tv';
-    return window.fetch(url).then(r => r.json()).then((d) => new Playlist(d.playlist));
-  }
-
-  /**
-   * @returns {Channel}
-   */
-  get currentChannel() {
-    return this.state.currentChannel || {};
-  }
-
-  get streamUrl() {
-    return (this.currentChannel.stream || '').replace('{KEY}', this.state.currentKey)
-  }
-
- /**
-  * @param {History} history
-  * @param {Channel} channel
-  * */
-  changeChannel(history, channel) {
-    this.setState({
-      currentChannel: channel,
-    });
-
-    history.push('/channel/' + channel.urlSlug);
-  }
-
-  saveFavourites(favourites){
-    this.favouritesStorage.set(favourites);
-    this.setState({favourites});
-  }
-
-  exitFromFavouritesEditor() {
-    this.setState({ currentChannelListType: ChannelListMode.all })
-  }
-
   render() {
-    const favouritesEditor = ({history}) => (
-      <FavouritesEditor channels={this.state.channels}
-                        favourites={this.state.favourites}
-                        onChangeChannel={this.changeChannel.bind(this)}
-                        onSave={(f) => {this.saveFavourites(f); history.push('/')}}
-                        onCancel={() => history.push('/')}/>);
+    const showCase = (routerProps) => {
+      const props = Object.assign({}, routerProps, {
+        currentKey: this.state.currentKey,
+        playlistUrl: this.state.playlistUrl,
+      });
 
-    const channelsPanel = ({history}) => (
-      <ChannelsPanel channels={this.state.channels}
-                     current={this.state.currentChannel}
-                     favourites={this.state.favourites}
-                     onChangeChannel={this.changeChannel.bind(this, history)}/>);
+      return <Showcase {...props}/>;
+    };
 
     return (
       <Router>
-        <div className={styles.app}>
-          <Route exact path="/" render={channelsPanel}/>
-          <Route exact path="/channel/:id" render={channelsPanel}/>
-
-          <Route exact path='/edit-favourites' render={favouritesEditor}/>
-
-          <div className={styles.mainPanel}>
-            {/*<VideoPlayer src={this.streamUrl}></VideoPlayer>*/}
-          </div>
+        <div>
+          <Route exact path="/" render={showCase}/>
+          <Route exact path="/channels/:channelSlug" render={showCase}/>
         </div>
       </Router>
     );
---
+++ src/components/showcase/showcase.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './showcase.scss';
+import { VideoPlayer } from '../video-player/video-player';
+import { Playlist } from '../../entities/playlist.model';
+import { FavouritesEditor } from '../favourites-editor/favourites-editor';
+import { ChannelsPanel } from '../channels-panel/channels-panel';
+import { Route, Switch } from 'react-router-dom'
+import { LocalStorageFactory } from '../../libs/storage';
+
+export class Showcase extends Component {
+  static propTypes = {
+    currentKey: PropTypes.string,
+    playlistUrl: PropTypes.string,
+  };
+
+  favouritesStorage = LocalStorageFactory.create('favourites');
+
+  state = {
+    currentChannel: null,
+    favourites: this.favouritesStorage.get([]),
+    channels: [],
+  };
+
+  componentWillMount() {
+    this.loadPlaylist().then((playlist) => {
+      this.setState({
+        channels: playlist.channels,
+        currentChannel: this.getInitialChannel(playlist.channels)
+      });
+    })
+  }
+
+  /**
+   * @returns {Promise.<Playlist>}
+   */
+  loadPlaylist() {
+    const url = this.props.playlistUrl;
+    return window.fetch(url).then(r => r.json()).then((d) => new Playlist(d.playlist));
+  }
+
+  /**
+   * @returns {Channel}
+   */
+  get currentChannel() {
+    return this.state.currentChannel || {};
+  }
+
+  get streamUrl() {
+    return (this.currentChannel.stream || '').replace('{KEY}', this.props.currentKey)
+  }
+
+  /**
+   * @param {Channel[]} channels
+   * @return Channel
+   */
+  getInitialChannel(channels) {
+    const { channelSlug } = this.props.match.params;
+
+    if (!channelSlug) {
+      return null;
+    }
+
+    const [id] = channelSlug.match(/^[^-]+/);
+
+    if (id) {
+      return channels.find((ch) => ch.id === id);
+    }
+  }
+
+ /**
+  * @param {History} history
+  * @param {Channel} channel
+  * */
+  changeChannel(history, channel) {
+    this.setState({
+      currentChannel: channel,
+    });
+
+    history.push('/channels/' + channel.urlSlug);
+  }
+
+  saveFavourites(favourites){
+    this.favouritesStorage.set(favourites);
+    this.setState({favourites});
+  }
+
+  render() {
+    const favouritesEditor = ({history}) => (
+      <FavouritesEditor channels={this.state.channels}
+                        favourites={this.state.favourites}
+                        onChangeChannel={this.changeChannel.bind(this)}
+                        onSave={(f) => {this.saveFavourites(f); history.push('/')}}
+                        onCancel={() => history.push('/')}/>);
+
+    const channelsPanel = ({history}) => (
+      <ChannelsPanel channels={this.state.channels}
+                     current={this.state.currentChannel}
+                     favourites={this.state.favourites}
+                     onChangeChannel={this.changeChannel.bind(this, history)}/>);
+
+    return (
+        <div className={styles.host}>
+          <Switch>
+            <Route exact path={`channels/edit-favourites`} render={favouritesEditor}/>
+            <Route path="/" render={channelsPanel}/>
+          </Switch>
+
+          <div className={styles.mainPanel}>
+            {/*<VideoPlayer src={this.streamUrl}></VideoPlayer>*/}
+          </div>
+        </div>
+    );
+  }
+}

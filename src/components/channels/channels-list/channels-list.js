1f1ab7f71e1d8b968b08c9caf0d8d3959acc704a
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-07-31 19:07:52 +0200

    add hardcoded markup for current EPG in channels list

--- src/components/channels/channels-list/channels-list.js
+++ src/components/channels/channels-list/channels-list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Channel } from '../../../entities/channel.model';
 import styles from './channels-list.scss';
+import barStyles from './progress-bar.scss';
 
 export class ChannelsList extends Component {
   static propTypes = {
@@ -29,7 +30,15 @@ export class ChannelsList extends Component {
             <div className={styles.icon}><img src={channel.logo} alt=""/></div>
             <div className={styles.details}>
               <h5 className={styles.name}>{channel.name}</h5>
-              <div className={styles.currentProgram}></div>
+              <div className={styles.currentProgram}>Охотник за головами (16+)</div>
+
+              <div className={barStyles.host}>
+                <div className={barStyles.startTime}>17.50</div>
+                <div className={barStyles.bar}>
+                  <div className={barStyles.barInner} style={{width: '25%'}}></div>
+                </div>
+                <div className={barStyles.endTime}>19.30</div>
+              </div>
             </div>
           </div>
         )}

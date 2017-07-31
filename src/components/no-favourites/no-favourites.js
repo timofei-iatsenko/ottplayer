d11c87d0c22fc6579bafc04ae8c40647eab4c798
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-07-31 16:21:09 +0200

    styles for no-favourites

--- src/components/no-favourites/no-favourites.js
+++ src/components/no-favourites/no-favourites.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Star from 'react-icons/lib/fa/star';
+import styles from './no-favourites.scss';
 
 export class NoFavourites extends Component {
   render() {
     return (
-      <div>
-        <div>Favorites are not set yet</div>
-        <Link to="/edit-favourites">Set Now..</Link>
+      <div className={styles.host}>
+        <div className={styles.noFavIcon}>
+          <Star />
+        </div>
+
+        <div className={styles.text}>
+          Favorites are not set yet <br />
+          <Link className={styles.action} to="/edit-favourites">Set Now..</Link>
+        </div>
+
       </div>
     );
   }

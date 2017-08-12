bed6adf6e353d004aa900bf4d29f7d84c7c1fdcb
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-08-12 16:10:26 +0200

    remove serviceworker as it doesn't register without https

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();

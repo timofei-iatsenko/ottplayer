6cd05fa68e37e27f7eef582edda4de8539f5b353
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-11-01 16:12:07 +0100

    remove dependency from backend, fetch playlist directly from provider

--- src/actions/playlist.actions.ts
+++ src/actions/playlist.actions.ts
@@ -1,7 +1,7 @@
 import { AppState } from '../store';
 import { Playlist } from '../entities/playlist.model';
 import { ThunkAction } from 'redux-thunk';
-import { Channel } from '../entities/channel.model';
+import * as Api from '../api/playlist.api';
 
 export const REQUEST_PLAYLIST = 'REQUEST_PLAYLIST';
 export const RECEIVE_PLAYLIST = 'RECEIVE_PLAYLIST';
@@ -25,16 +25,8 @@ export function fetchPlaylist(playlistUrl: string): ThunkAction<Promise<any>, Ap
   return (dispatch) => {
     dispatch(requestPlaylist(playlistUrl));
 
-    return window.fetch(playlistUrl)
-      .then((r) => r.json())
-      .then((d) => d.playlist)
-      .then((playlist: Playlist) => {
-        playlist.channels.forEach((c: Channel) => {
-          c.logo = playlist.urlLogo + c.logo;
-          c.id = +c.id;
-        });
-
-        return dispatch(receivePlaylist(playlist));
-      });
+    return Api.fetchPlaylist(playlistUrl).then((playlist: Playlist) => {
+      return dispatch(receivePlaylist(playlist));
+    });
   };
 }
--- src/reducers/settings.reducer.ts
+++ src/reducers/settings.reducer.ts
@@ -1,7 +1,7 @@
 import { Settings } from '../entities/settings.model';
 
 const initialState = {
-  playlistUrl: '/api/playlist?url=myott.tv',
+  playlistUrl: 'myott.tv',
   currentKey: '00XE8DMEI7',
 };
 

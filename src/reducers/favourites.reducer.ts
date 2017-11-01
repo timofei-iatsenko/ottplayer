3e73a2fd8f14e3f7547ecf642aeef2879eaa09b0
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-11-01 20:55:12 +0100

    fix favourites editor
    store state in component instead of redux, because it's useful in that case

--- src/reducers/favourites.reducer.ts
+++ src/reducers/favourites.reducer.ts
@@ -2,20 +2,16 @@ import { FavouritesActions } from '../actions/favourites.actions';
 
 export interface FavouritesState {
   readonly savedChannels: ReadonlyArray<number>;
-  readonly selectedChannels: ReadonlyArray<number>;
 }
 
 const initialState: FavouritesState = {
   savedChannels: [],
-  selectedChannels: [],
 };
 
 export function favouritesReducer(state = initialState, action: any): FavouritesState {
   switch (action.type) {
     case FavouritesActions.save:
-      return {...state, savedChannels: action.favourites, selectedChannels: action.favourites};
-    case FavouritesActions.select:
-      return {...state, selectedChannels: action.favourites};
+      return {...state, savedChannels: action.favourites};
     default:
       return state;
   }

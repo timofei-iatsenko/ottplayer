92ac245a50541f3785bc392409534cafc3e25d21
Timofey_Yatsenko <timofey_yatsenko@epam.com>
2017-11-02 11:44:44 +0100

    stringify injected vars

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -142,7 +142,7 @@ module.exports = {
    */
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': process.env.NODE_ENV,
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     }),
     new CheckerPlugin(),
 

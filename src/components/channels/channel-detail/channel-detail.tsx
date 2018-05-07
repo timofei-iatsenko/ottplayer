54dbd8db9b87bc25557eedd9994a64aa462f9eaa
Timofei Iatsenko <timofei_iatsenko@epam.com>
2018-05-07 23:19:12 +0200

    tune styles for mobile

--- src/components/channels/channel-detail/channel-detail.tsx
+++ src/components/channels/channel-detail/channel-detail.tsx
@@ -4,6 +4,7 @@ import { Channel } from '../../../entities/channel.model';
 import { EpgEntry } from '../../../entities/epg-entry';
 import { AppState } from '../../../store';
 import { epgInAir } from '../../../store/reducers/epg.reducer';
+import { Time } from '../../formatters/time';
 import { ProgressBar } from '../../progress-bar/progress-bar';
 import styles from './channels-detail.scss';
 
@@ -48,10 +49,20 @@ class ChannelDetailComponent extends PureComponent<Props> {
     return (<div className={styles.details}>
       <h5 title={this.props.channel.name} className={styles.name}>{this.props.channel.name}</h5>
       {epg.current && <div title={'Сейчас: ' + epg.current.name}
-                   className={styles.currentProgram}>{epg.current.name}</div>}
-      {epg.current && <ProgressBar startTime={epg.current.startTime} endTime={epg.current.endTime}/>}
+                           className={styles.currentProgram}>
+        <span className={styles.time}><Time>{epg.current.startTime}</Time></span>
+        {epg.current.name}
+        </div>}
+      {epg.current &&
+      <ProgressBar startTime={epg.current.startTime} endTime={epg.current.endTime}/>
+      }
+
       {epg.next &&
-      <div title={'Затем: ' + epg.next.name} className={styles.nextProgram}>Затем: {epg.next.name}</div>}
+      <div title={'Далее: ' + epg.next.name} className={styles.nextProgram}>
+        <span className={styles.time}><Time>{epg.next.startTime}</Time></span>
+        {epg.next.name}
+      </div>
+      }
     </div>);
   }
 }
